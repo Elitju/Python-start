799716e7a11a1705755efbffd940f270842a1a72
Elitju <122849541+Elitju@users.noreply.github.com>
2024-10-08 23:09:23 +0100

    Add files via upload
    day 1 of trying out stuff and seeing how github kinda works, thinking of privating for a bit temporarily?

---
+++ day1.py
@@ -0,0 +1,26 @@
+#Testing list manipulation
+
+list = [0,1,2,3,4,5,6,7,8,9,10,11]
+print(list[2:4])
+# starting from 0, runs number 2 and 3 but not 4
+print(list[2:-5])
+#similarly 2 to -6 not -5, where - is starting count from right
+print(len(list))
+#len displays correct number of lenght of list, what did i think would happen, actually no im trying to really solidify fundementals idc if it sounds stupid
+
+print(list[::-1])
+#reverses list
+
+list_c = ['red','green','blue','yellow']
+print(list_c.pop())
+print(list_c)
+#removed last item for rest of code, not permanent
+
+print(list.reverse())
+#none??
+
+print(list_c.reverse())
+#also none..?
+
+#will figure out more day 2
+
